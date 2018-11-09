521f40534e1c7d43e03cd90d857dc7c4b4b73554
Your Name <you@example.com>
2018-11-09 19:14:56 +0100

    Form validation
    Add validation to name, email and list fields. Create .errors class in main.css

--- scripts.js
+++ scripts.js
@@ -33,4 +33,55 @@ for (var i = 0; i < box.length; i++){
 }
 }
 }
+// walidator formularza
+var form = document.querySelector(".form");
+    fields = form.querySelectorAll("[data-error]");
+    dane = form.querySelector(".dane");
 
+function isNotEmpty(field){
+    return field.value !== "";
+};
+function isEmail(field){
+    return field.value.indexOf("@") !== -1;
+};
+function displayErrors(errors){
+    var ul = document.querySelector("ul.errors")
+    if(!ul){
+        ul = document.createElement("ul");
+        ul.classList.add("errors");
+    }
+    ul.innerHTML = "";
+    errors.forEach(function(error){
+        var li = document.createElement("li");
+        li.textContent = error;
+        ul.appendChild(li);
+    });
+    dane.appendChild(ul);
+};
+form.addEventListener("submit", function(e){
+    var errors = [];
+    e.preventDefault();
+    for(let i = 0; i < fields.length; i++){
+        var field = fields[i],
+        isValid = false;
+        if(field.type === "text"){
+        isValid = isNotEmpty(field);
+        }else if (field.type === "email"){
+        isValid = isEmail(field);
+        }else if(field.type === "select-one"){
+        isValid = isNotEmpty(field);
+        }
+        if(!isValid){
+        field.classList.add("error");
+        errors.push(field.dataset.error);
+        }else{
+        field.classList.remove("error");
+    }
+}
+    if(errors.length){
+        displayErrors(errors);
+    }else{
+        form.submit();
+    }
+    console.log(errors);
+}, false);
